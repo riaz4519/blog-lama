04ec6eca3377f97e3b7c1f0b8a3525eb895d9d23
riaz4519 <riaz.i3216@gmail.com>
2022-01-15 22:04:59 +0600

    after finishing api part

--- api/index.js
+++ api/index.js
@@ -3,14 +3,43 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
+const multer = require("multer");
 
 dotenv.config();
+app.use(express.json());
 
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log("connected to mongo"))
   .catch((error) => console.error(error));
 
-app.listen("5000", () => {
-  console.log("Backend is running");
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null,req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+app.post("/api/upload",upload.single("file"),(req,res) =>{
+
+  res.status(200).json("File has been uploaded");
+
+});
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
+
+const port = 5000;
+app.listen(port, () => {
+  console.log("Backend is running : " + port);
 });
---
+++ api/routes/categories.js
@@ -0,0 +1,33 @@
+const router = require("express").Router();
+const Category = require("../models/Category");
+
+//CREATE
+router.post('/',async(req,res) =>{
+
+    const newCategory = new Category(req.body);
+    try{
+        const savedCat = await newCategory.save();
+        res.status(200).json(savedCat);
+
+    }catch(err){
+        res.status(500).json(err);
+    }
+
+});
+
+//GET ALL
+router.get('/',async(req,res) =>{
+
+    try{
+
+        const categories = await Category.find();
+        res.status(200).json(categories);
+
+    }catch(err){
+        
+        res.status(500).json(err);
+    }
+
+});
+
+module.exports = router;
